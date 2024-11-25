3e40083a4bdefeaf29b606edf0c4b08dda71fbc9
kirketerp <jkan67729@edu.ucl.dk>
2024-11-25 12:26:27 +0100

    added cw!

--- testgit/testgit/Program.cs
+++ testgit/testgit/Program.cs
@@ -4,8 +4,11 @@
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Also new Text");
+
+            Console.WriteLine("Hej");
         }
     }
 }
